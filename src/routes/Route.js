fb6f09be3f5f86f82081892b3a0baa91a87fc40e
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-08-07 08:11:14 -0300

    NEW - Configuração do Root Import

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
-import AuthLayout from '../layouts/auth';
-import DefaultLayout from '../layouts/default';
+import AuthLayout from '~/layouts/auth';
+import DefaultLayout from '~/layouts/default';
 
 export default function RouteWrapper({
   component: Component,
