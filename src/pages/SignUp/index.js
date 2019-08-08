4516c3f356dc2ced7ff52902ced7e70755e96aa0
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-08-07 23:07:28 -0300

    NEW - Estilização do layout de autenticação

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.svg';
 
 // import { Container } from './styles';
 
-export default function SignUp() {
-  return <div />;
+export default function SignUn() {
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form action="">
+        <input placeholder="Nome completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
