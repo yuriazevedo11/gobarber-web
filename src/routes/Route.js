23a7f4f740697fa582e8f4e87f4c6caa0b1d473f
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-08-10 11:24:18 -0300

    NEW - Persistência da autenticação

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -5,7 +5,7 @@ import { Route, Redirect } from 'react-router-dom';
 import AuthLayout from '~/layouts/auth';
 import DefaultLayout from '~/layouts/default';
 
-import store from '~/store';
+import { store } from '~/store';
 
 export default function RouteWrapper({
   component: Component,
