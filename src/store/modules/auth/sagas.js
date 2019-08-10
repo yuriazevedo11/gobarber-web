db968a4a4a30916f0ddf4993ecf30e46d290d14b
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-08-10 12:06:23 -0300

    NEW - Requisições autenticadas

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -22,13 +22,49 @@ export function* signIn({ payload }) {
       return;
     }
 
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
     yield put(signInSuccess(token, user));
 
     history.push('dashboard');
   } catch (err) {
-    toast.error('Falha na autenticação, verifique os seus dados');
+    toast.error('Falha na autenticação, verifique seus dados');
+
     yield put(signFailure());
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signup({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    history.push('/');
+  } catch (err) {
+    toast.error('Falha no cadastro, verifique seus dados');
+
+    yield put(signFailure());
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signup),
+]);
