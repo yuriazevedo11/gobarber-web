4d486c27db1980adccbf221773e7d2954fb88774
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-08-10 17:15:43 -0300

    NEW - Página de perfil

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -1,19 +1,9 @@
 import styled from 'styled-components';
 import { darken } from 'polished';
 
-export const Wrapper = styled.div`
-  height: 100%;
-  background: linear-gradient(-90deg, #7159c1, #ab59c1);
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-export const Content = styled.div`
-  width: 100%;
-  max-width: 315px;
-  text-align: center;
+export const Container = styled.div`
+  max-width: 600px;
+  margin: 50px auto;
 
   form {
     display: flex;
@@ -40,6 +30,13 @@ export const Content = styled.div`
       margin-bottom: 10px;
     }
 
+    hr {
+      border: 0;
+      height: 1px;
+      background: rgba(255, 255, 255, 0.2);
+      margin: 10px 0 20px;
+    }
+
     button {
       margin: 5px 0 0;
       height: 44px;
@@ -57,14 +54,20 @@ export const Content = styled.div`
     }
   }
 
-  a {
+  > button {
+    width: 100%;
+    margin: 10px 0 0;
+    height: 44px;
+    background: #f64c75;
+    font-weight: bold;
     color: #fff;
-    margin-top: 15px;
+    border: 0;
+    border-radius: 4px;
     font-size: 16px;
-    opacity: 0.8;
+    transition: background 0.3s;
 
     &:hover {
-      opacity: 1;
+      background: ${darken(0.07, '#f64c75')};
     }
   }
 `;
